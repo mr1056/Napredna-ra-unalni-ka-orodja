430e7d3cbd0944f5caf8053dec3293f29b24f8bb
mr1056 <146850912+mr1056@users.noreply.github.com>
2023-11-13 15:52:51 +0100

    Add files via upload

---
+++ Domača_naloga_2/MonteCfunkcija.m
@@ -0,0 +1,16 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*MonteC[n_]:=Module[{tocke,Tockeznotraj},tocke=RandomReal[{-1,1},{n,2}];Tockeznotraj=Select[tocke,Norm[#]<=1&];4 Length[Tockeznotraj]/Length[tocke]]*)
+
+
+(* ::Input:: *)
+(**)
+
+
+(* ::Input:: *)
+(**)
+
+
+(* ::Input:: *)
+(**)
